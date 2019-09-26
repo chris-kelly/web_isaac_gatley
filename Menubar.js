358355d2e1ca0c5ba021c1528da6db3b9fdae485
chris-kelly <chris.kelly@deliveroo.co.uk>
2019-09-26 16:17:44 +0100

    Menu button updates (mobile)

--- Menubar.js
+++ Menubar.js
@@ -1,22 +1,36 @@
-﻿		function openSubmenu() {
-		    var v = document.getElementById("submenu");
-			    if (v.className != "no_show")
-			       {v.className = "no_show"}
-			    else {v.className = "show"}
-			var w = document.getElementById("rotate1");
-			    if (w.className != "rotatesmall")
-			       {w.className = "rotatesmall"}
-			    else {w.className = "rotateoff"}
-		    var x = document.getElementById("rotate2");
-			    if (x.className != "rotatebig")
-			       {x.className = "rotatebig"}
-			    else {x.className = "rotateoff"}
-		    var y = document.getElementById("top");
-			    if (y.className != "not_visible")
-			       {y.className = "not_visible"}
-			    else {y.className = "visible"}
-		    var z = document.getElementById("bottom");
-			    if (z.className != "not_visible")
-			       {z.className = "not_visible"}
-			    else {z.className = "visible"}  
-			}
+﻿function openSubmenu() {
+
+	var rC = document.getElementById("rotateClockwise");
+	var rA = document.getElementById("rotateAnticlockwise");
+	var lT = document.getElementById("top");
+	var lB = document.getElementById("bottom");
+	var s = document.getElementById("submenu");
+
+		if (rC.style.transform != "rotate(45deg)") { // if not already rotated
+			// rotate middle 2
+			rC.style.transform = "rotate(45deg)"
+			rA.style.transform = "rotate(-45deg)"
+			// make middle 2 dark
+			rC.style.backgroundColor = "black"
+			rA.style.backgroundColor = "black"
+			// hide top/bottom grey
+			lT.style.opacity = 0
+			lB.style.opacity = 0
+			// show submenu
+			s.style.height = "65vw"
+			s.style.opacity = "0.95"
+		} else {
+			// make middle 2 horizontal
+	    	rC.style.transform = "rotate(0deg)"
+	    	rA.style.transform = "rotate(0deg)"
+	    	// make middle 2 grey
+	    	rC.style.backgroundColor = "#B0B0B0"
+			rA.style.backgroundColor = "#B0B0B0"
+			// show top/bottom grey
+			lT.style.opacity = 1
+			lB.style.opacity = 1
+			// hide submenu
+			s.style.height = "0vw"
+			s.style.opacity = "0"
+	    }
+}
